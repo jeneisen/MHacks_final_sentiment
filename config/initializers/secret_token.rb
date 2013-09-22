372fd995ef7d8e0e27aa10511aee80fe168c2a91
Jen E <jeneisenberg5@gmail.com>
2013-09-22 00:09:26 -0400

    legit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MhacksFinalSentiment::Application.config.secret_token = 'fa0bb1dbfb7dd1647675e9bf5210dd2fde451ee1ebcbde99bffca8b797c5541fcac1ccf9b2ef212e57d320a4dbb05c63bd4c056f1c457216f40a9bbe3c08e239'
