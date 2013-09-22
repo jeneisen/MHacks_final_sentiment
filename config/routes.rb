c39c2344ee831eafe30e7804e3d72900e607fd5e
Jen E <jeneisenberg5@gmail.com>
2013-09-22 02:10:57 -0400

    static page controller page working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 MhacksFinalSentiment::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
+  resources :page
+  root :to =>'page#home'
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
@@ -48,7 +50,7 @@ MhacksFinalSentiment::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
